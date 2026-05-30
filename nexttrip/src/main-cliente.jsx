23522972f50bb0aa0eb2940da11a140ba8091bca
SebastianEchevarria <carnevale1989alberto@gmail.com>
2026-05-30 20:31:25 +0200

    Delete nexttrip directory

--- nexttrip/src/main-cliente.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import NextTripClientApp from './cliente-app.jsx'
-
-const root = createRoot(document.getElementById('root'))
-root.render(<NextTripClientApp />)
-document.getElementById('loading')?.classList.add('hidden')
--- nexttrip/src/main-driver.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import RivieraApp from './riviera-app.jsx'
-
-const root = createRoot(document.getElementById('root'))
-root.render(<RivieraApp />)
-document.getElementById('loading')?.classList.add('hidden')
