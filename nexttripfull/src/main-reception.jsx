654a493816594a71ef6dff877be96d95e1219c0e
SebastianEchevarria <carnevale1989alberto@gmail.com>
2026-06-09 14:35:08 +0200

    Delete nexttripfull directory

--- nexttripfull/src/main-reception.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import RivieraApp from './riviera-app.jsx'
-
-const root = createRoot(document.getElementById('root'))
-root.render(<RivieraApp initialRole="reception" />)
-document.getElementById('loading')?.classList.add('hidden')
